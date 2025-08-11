cbbd70b4950b0d514990a65820fb3fed80cecd9f
alvxp <77099211+alvxp@users.noreply.github.com>
2025-08-11 23:22:08 +0800

    Update to models

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,18 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference
 
 # Add the necessary imports for the starter code.
+import os
+import pandas as pd
+import pickle
+
 
 # Add code to load in the data.
+file_path = "../data/census.csv"
+data = pd.read_csv(file_path)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -23,6 +31,21 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb)
 
 # Train and save a model.
+new_model = train_model(X_train, y_train)
+
+model_path = os.path.join(file_path, "./model/trained_model.pkl")
+pickle.dump(new_model, open(model_path, "wb"))
+
+encoder_path = os.path.join(file_path, "./model/encoder.pkl")
+pickle.dump(new_model, open(encoder_path, "wb"))
+
+lb_path = os.path.join(file_path, "./model/lb.pkl")
+pickle.dump(new_model, open(lb_path, "wb"))
+
+preds = inference(new_model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
