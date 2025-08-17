5eda9839a5859d9a024fadb24ed9215654166ce6
alvxp <77099211+alvxp@users.noreply.github.com>
2025-08-18 06:43:01 +0800

    Minor amendments to file paths

--- starter/train_model.py
+++ starter/train_model.py
@@ -1,8 +1,8 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
-from starter.ml.data import process_data
-from starter.ml.model import train_model, compute_model_metrics, inference, compute_slice_metrics
+from starter.data import process_data
+from starter.model import train_model, compute_model_metrics, inference, compute_slice_metrics
 
 # Add the necessary imports for the starter code.
 import os
