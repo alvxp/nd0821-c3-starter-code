342d557b224640ad5daec3eb767557a3f3e1c2d0
alvxp <77099211+alvxp@users.noreply.github.com>
2025-08-17 19:02:46 +0800

    Amendments

--- starter/main.py
+++ starter/main.py
@@ -50,12 +50,12 @@ async def predict(data: InputData):
     cat_features = [
         "workclass",
         "education",
-        "marital-status",
+        "marital_status",
         "occupation",
         "relationship",
         "race",
         "sex",
-        "native-country",
+        "native_country",
     ]
 
     sample = pd.DataFrame([data.dict()])
