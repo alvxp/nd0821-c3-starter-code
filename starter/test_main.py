ab9b391831488049ce56e967e4820940ebdb8910
alvxp <77099211+alvxp@users.noreply.github.com>
2025-08-17 20:34:03 +0800

    Minor amendments.

--- starter/test_main.py
+++ starter/test_main.py
@@ -2,7 +2,7 @@ import os, sys
 import json
 from fastapi.testclient import TestClient
 
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__), "starter"))
 
 from main import app
 
